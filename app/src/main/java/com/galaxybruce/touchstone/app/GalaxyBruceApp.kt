0b4a42312b3bf5aa1898e4acb2906876e025dcd1
bruce <brucezhanggood@gmail.com>
2019-06-22 10:58:15 +0800

    Application

---
+++ app/src/main/java/com/galaxybruce/touchstone/app/GalaxyBruceApp.kt
@@ -0,0 +1,15 @@
+package com.galaxybruce.touchstone.app
+
+import com.galaxybruce.component.app.BaseApplication
+
+/**
+ * @date 2019-06-22 10:41
+ * @author bruce.zhang
+ * @description
+ *
+ * modification history:
+ */
+
+class GalaxyBruceApp : BaseApplication() {
+
+}
