ff7c20bd33a77648d75c29a5a7d18b3c3b153fba
bruce <brucezhanggood@gmail.com>
2022-01-27 17:23:54 +0800

    给view实现ViewLifecycleOwner

--- component/src/main/java/com/galaxybruce/component/util/RxLifecycleUtils.java
+++ component/src/main/java/com/galaxybruce/component/util/RxLifecycleUtils.java
@@ -11,6 +11,10 @@ import androidx.lifecycle.LifecycleOwner;
  * @author bruce.zhang
  * @date 2022/1/27 16:30
  * @description rx生命周期相关
+ *
+ * 可以给view实现ViewLifecycleOwner：
+ * [一个简单非侵入式的AutoDispose](https://juejin.cn/post/6872337518163918861)
+ *
  * <p>
  * modification history:
  */
