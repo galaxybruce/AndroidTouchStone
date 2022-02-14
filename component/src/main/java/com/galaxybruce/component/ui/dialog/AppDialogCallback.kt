10e774256c3b2e09ea0999f01b63d5f1e40f32b4
bruce <brucezhanggood@gmail.com>
2022-02-14 10:55:02 +0800

    AppDialogCallback

--- component/src/main/java/com/galaxybruce/component/ui/dialog/AppDialogCallback.kt
+++ component/src/main/java/com/galaxybruce/component/ui/dialog/AppDialogCallback.kt
@@ -7,7 +7,7 @@ import android.os.Parcelable
  * @date 2022/2/14 10:34
  * @author bruce.zhang
  * @description dialog callback 父类，用于防止旋转屏幕等callback丢失
- * 也可以参考ResultReciver实现
+ * 也可以参考ResultReciver实现 或者直接使用ResultReciver来当做callback
  *
  * modification history:
  */
