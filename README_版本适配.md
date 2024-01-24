dafa1724444c97e4e5656ee94ea36268d10ff967
bruce <brucezhanggood@gmail.com>
2024-01-24 17:45:09 +0800

    多个连续请求中有弹窗  优化

---
+++ README_版本适配.md
@@ -0,0 +1,5 @@
+# 参考文章
+[Android 版本适配全套指南](https://github.com/getActivity/AndroidVersionAdapter/blob/master/README.md)
+
+
+
--- module_main/src/main/java/com/galaxybruce/main/ui/mvvm/request/MainRequest.kt
+++ module_main/src/main/java/com/galaxybruce/main/ui/mvvm/request/MainRequest.kt
@@ -2,17 +2,13 @@ package com.galaxybruce.main.ui.mvvm.request
 
 import android.annotation.SuppressLint
 import android.content.Context
-import androidx.lifecycle.viewModelScope
 import com.galaxybruce.component.ui.dialog.AppConfirmDialog
-import com.galaxybruce.component.ui.jetpack.JPBaseRequest
+import com.galaxybruce.component.ui.jetpack.JPBaseRequestV2
 import com.galaxybruce.main.ui.mvvm.viewmodel.MainViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 
-class MainRequest(private val viewModel: MainViewModel) : JPBaseRequest() {
+class MainRequest(private val viewModel: MainViewModel) : JPBaseRequestV2(viewModel) {
 
 //    private val mApi: LiveApi = KRetrofitFactory.createService(LiveApi::class.java)
 
@@ -31,22 +27,47 @@ class MainRequest(private val viewModel: MainViewModel) : JPBaseRequest() {
 //                this.viewModel.title.set("返回数据失败")
 //            })
 
-        viewModel.viewModelScope.launch {
-            val r1 = request()
-            val input = showInputDialog(context)
-            request2(input.getOrElse { "输入出现异常" })
-        }
+
+        doTask(
+            block = {
+                // 多个连续请求中有弹窗
+                val r1 = request()
+
+                val input = showInputDialog(context)
+
+                val r2 = request2(input.getOrThrow()) // input.getOrElse { "输入出现异常" }
+                r2
+            },
+            successCallback = { result ->
+                showToast(result)
+            },
+            errorCallback = {
+                showToast(it.message)
+            },
+            showLoading = false
+        )
     }
 
-    private suspend fun request(): String = withContext(Dispatchers.IO) {
-        // 在这里执行接口请求操作，返回结果
-        "Result from request"
+    private suspend fun request(): String {
+        showLoadingProgress("请求1")
+        val result = withContext(Dispatchers.IO) {
+            delay(1000)
+            // 在这里执行接口请求操作，返回结果
+            "Result from request1"
+        }
+        hideLoadingProgress()
+        return result
     }
 
-    private suspend fun request2(text: String) {
-        viewModel.viewModelScope.launch {
-            showToast(text)
+    private suspend fun request2(text: String): String {
+        showLoadingProgress("请求2")
+        val result = withContext(Dispatchers.IO) {
+            delay(1000)
+            // 在这里执行接口请求操作，返回结果
+            "Result from request2: ${text}"
         }
+        hideLoadingProgress()
+        return result
     }
 
     /**
@@ -57,7 +78,8 @@ class MainRequest(private val viewModel: MainViewModel) : JPBaseRequest() {
     private suspend fun showInputDialog(context: Context): Result<String> {
         return suspendCancellableCoroutine { continuation ->
             continuation.invokeOnCancellation {
-                // todo 协程被取消的处理
+                // 协程被取消的处理
+                continuation.resume(Result.failure(it ?: Exception("任务已取消")), null)
             }
 
             AppConfirmDialog.create("提示",
